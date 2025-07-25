b363d7170514e63f8c2f122c33306979e3d2db65
raj <raj@gmail.com>
2025-07-25 11:08:30 +0000

    v6

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def home():
         <h1>🚀 Hello from Rajveer & Vikas and Amit sir Flask App!</h1>
         <h2>Deployed via GitHub Actions CI/CD to Azure</h2>
         <div class="version">
-            ✅ Deployment Version: <strong>v1.0.1</strong>
+            ✅ Deployment Version: <strong>v1.0.2</strong>
         </div>
     </body>
     </html>
